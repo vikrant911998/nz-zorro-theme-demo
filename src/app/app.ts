7978e4ea444ebc88b5ed79ed57b3113844fc0bd3
Vikrant Singh <vikrantbhadauriya2@gmail.com>
2026-01-13 17:34:24 +0530

    [Update] added color picker

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzColorPickerModule } from 'ng-zorro-antd/color-picker';
+import { NzDividerComponent } from 'ng-zorro-antd/divider';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzColDirective, NzRowDirective } from 'ng-zorro-antd/grid';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NzColorPickerModule,
+    NzRowDirective,
+    NzColDirective,
+    NzDividerComponent,
+    ReactiveFormsModule,
+    NzButtonModule,
+    NzFormModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('nz-zorro-theme-demo');
+  private formBuilder = inject(FormBuilder);
+
+  colorForm = this.formBuilder.group({
+    colorPicker: ['#1677ff']
+  });
+
+  submitForm(): void {
+    console.log(this.colorForm.value)
+  }
 }
