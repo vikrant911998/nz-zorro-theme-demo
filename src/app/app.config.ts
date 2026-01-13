85d70a40469045368663a88ea2bda79f4418ee87
Vikrant Singh <vikrantbhadauriya2@gmail.com>
2026-01-13 18:26:50 +0530

    [Update] added button, select, dropdown, alert, and input component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,13 +7,28 @@ import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { NzConfig, provideNzConfig } from 'ng-zorro-antd/core/config';
 
 registerLocaleData(en);
+
+const ngZorroConfig: NzConfig = {
+  theme: {
+    primaryColor: '#3a6ef2',
+    successColor: '#12b76a',
+    errorColor: '#f04438',
+    warningColor: '#f79009',
+    infoColor: '#0dcaf0',
+  }
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideNzI18n(en_US), provideAnimationsAsync(), provideHttpClient()
+    provideRouter(routes),
+    provideNzI18n(en_US),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    provideNzConfig(ngZorroConfig)
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzColorPickerModule } from 'ng-zorro-antd/color-picker';
+import { NzConfigService } from 'ng-zorro-antd/core/config';
 import { NzDividerComponent } from 'ng-zorro-antd/divider';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzColDirective, NzRowDirective } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule, NzSelectOptionInterface } from 'ng-zorro-antd/select';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +22,14 @@ import { NzColDirective, NzRowDirective } from 'ng-zorro-antd/grid';
     NzDividerComponent,
     ReactiveFormsModule,
     NzButtonModule,
-    NzFormModule
+    NzFormModule,
+    NzSelectModule,
+    FormsModule,
+    NzAlertModule,
+    NzDropDownModule,
+    NzIconModule,
+    NzInputModule,
+    NzSpaceModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
@@ -23,12 +37,25 @@ import { NzColDirective, NzRowDirective } from 'ng-zorro-antd/grid';
 export class App {
   protected readonly title = signal('nz-zorro-theme-demo');
   private formBuilder = inject(FormBuilder);
+  private nzConfigService = inject(NzConfigService);
+  options: NzSelectOptionInterface[] = [
+    {
+      value: 'zhejiang',
+      label: 'Zhejiang'
+    },
+    {
+      value: 'jiangsu',
+      label: 'Jiangsu'
+    }
+  ];
 
   colorForm = this.formBuilder.group({
-    colorPicker: ['#1677ff']
+    colorPicker: ['#3a6ef2']
   });
 
   submitForm(): void {
-    console.log(this.colorForm.value)
+    this.nzConfigService.set('theme', {
+      primaryColor: this.colorForm.value.colorPicker as string
+    });
   }
 }
